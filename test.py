f6b6f71c62a942eae432cd86d1fd1c681e0a8dba
nirmoru <nyxvermilion47@gmail.com>
2023-02-23 20:25:26 -0500

    updated assertEqual to assertTrue

--- test.py
+++ test.py
@@ -7,29 +7,34 @@ class TestScapyMain(unittest.TestCase):
 		cls.packet = scapy_main.packet_sniffer()
 	
 	def setUp(self) -> None:
-		print("\n{} \nSet up".format(self.id().split('.')[-1]))
+		print("Starting Setup for {}.".format(self.id().split('.')[-1]))
+		print("Setup Complete.")
 
 	def tearDown(self) -> None:
-		print("Tearing down")
-
+			print("Starting Teardown.")
+			print("Tearing complete for {}.".format(self.id().split('.')[-1]))
+			print("-------------------------\n")
 
 	def test_PacketSniffer(self):
-		if self.packet  is None:
-			self.assertEqual(isinstance(self.packet , type(None)), True)
-		else:
-			self.assertEqual(isinstance(self.packet , dict), True)
-
+			if self.packet  is None:
+				self.assertTrue(isinstance(self.packet , type(None)))
+			else:
+				self.assertTrue(isinstance(self.packet , dict))
 
 	def test_PacketDecoding(self):
 		if self.packet  is None:
-			self.assertEqual(isinstance(self.packet , type(None)), True)
+			self.assertTrue(isinstance(self.packet , type(None)))
 		else:
-			self.assertEqual(isinstance(scapy_main.packet_decoding(self.packet .get('ActualPayload')), str), True)
+			self.assertTrue(isinstance(scapy_main.packet_decoding(self.packet .get('ActualPayload')), str))
 
 	def test_PacketManipulation(self):
 		data = ['127.0.0.1', '127.0.0.1', 8080, 8081, 'hello']
-		self.assertEqual(scapy_main.packet_manipulation(data), True)
+		self.assertTrue(scapy_main.packet_manipulation(data))
 		
 
 if __name__ == "__main__":
-	unittest.main()
+	test_order = ["test_PacketSniffer", "test_PacketDecoding", "test_PacketManipulation"]
+	test_loader = unittest.TestLoader()
+	test_loader.sortTestMethodsUsing = lambda x, y: test_order.index(x) - test_order.index(y)
+	unittest.main(testLoader=test_loader)
+
